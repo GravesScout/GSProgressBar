c3e81080d5fe55f92c5507c20d6b4cbdc0aee254
Dor Ditchi <dditchi@digibank.co.il>
2023-12-21 15:51:30 +0200

    first commit

---
+++ GSProgressBarExample/GSProgressBarExample/GSProgressBarExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GSProgressBarExampleApp.swift
+//  GSProgressBarExample
+//
+//  Created by Dor Ditchi on 21/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct GSProgressBarExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Sources/GSProgressBar/GSProgressBar.swift
@@ -0,0 +1,15 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+
+import SwiftUI
+
+public struct GSProgressBar: View {
+    public var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
