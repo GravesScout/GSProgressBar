43bb5f8ecc990ca452b18e72dbd3445b0a7a86f4
Dor Ditchi <dditchi@digibank.co.il>
2023-12-27 23:39:10 +0200

    Updated example app.

--- GSProgressBarExample/GSProgressBarExample/ContentView.swift
+++ GSProgressBarExample/GSProgressBarExample/ContentView.swift
@@ -12,12 +12,12 @@ struct ContentView: View {
     @State var progress: CGFloat = 0.0
     var body: some View {
         ZStack {
-            GSProgressBar(type: .circular, sectionsDurations: [
+            GSProgressBar(type: .circular, animationType: .sectioned(sections: [
                 .init(duration: 3, sectionProportionValue: 0.3, sectionDelay: 2),
                     .init(duration: 1.5, sectionProportionValue: 0.6, sectionDelay: 4),
-                .init(duration: 5, sectionProportionValue: 0.1)]) { updatedProgress in
+                    .init(duration: 5, sectionProportionValue: 0.1)]))
+            { updatedProgress in
                     progress = updatedProgress
-                    
                 }
             .frame(width: 150, height: 150)
             Text("\(progress)")
@@ -26,6 +26,120 @@ struct ContentView: View {
     }
 }
 
+enum CircularScreens {
+    case linear
+    case sectioned
+    case randomizedNoDelay
+    case randomizedConstantDelay
+    case randomizedRandomDelay
+}
+
+enum LoadersScreens {
+    case circular
+}
+
+struct ContentView2: View {
+    @State var path = NavigationPath()
+    var body: some View {
+        NavigationStack(path: $path) {
+            List {
+                NavigationLink(value: LoadersScreens.circular) {
+                    Text("Circular loader")
+                }
+            }
+            .navigationDestination(for: LoadersScreens.self) { screen in
+                switch screen {
+                case .circular:
+                    CircularScreensView()
+                }
+            }
+        }
+    }
+}
+
+
+struct CircularScreensView: View {
+    @State var progress: CGFloat = 0.0
+//    init(){
+//        print("In Init!")
+//    }
+    var body: some View {
+        List {
+            NavigationLink(value: CircularScreens.linear) {
+                Text("Linear loading")
+            }
+            NavigationLink(value: CircularScreens.sectioned) {
+                Text("Sectioned loading")
+            }
+            NavigationLink(value: CircularScreens.randomizedNoDelay) {
+                Text("Randomized no delay loading")
+            }
+            NavigationLink(value: CircularScreens.randomizedConstantDelay) {
+                Text("Randomized constant delay loading")
+            }
+            NavigationLink(value: CircularScreens.randomizedRandomDelay) {
+                Text("Randomized random delay loading")
+            }
+        }
+        .navigationDestination(for: CircularScreens.self) { screen in
+            switch screen {
+            case .linear:
+                CircularScreen()
+            case .sectioned:
+                ZStack {
+                    GSProgressBar(type: .circular, animationType: .sectioned(sections: [
+                        .init(duration: 3, sectionProportionValue: 0.3, sectionDelay: 2),
+                        .init(duration: 1.5, sectionProportionValue: 0.6, sectionDelay: 4),
+                        .init(duration: 5, sectionProportionValue: 0.1)])) { updatedProgress in
+                            progress = updatedProgress
+                        }
+                    .frame(width: 150, height: 150)
+                    Text("\(progress)")
+                }
+            case .randomizedNoDelay:
+                ZStack {
+                    GSProgressBar(type: .circular, animationType: .randomized(configuration: .init(sectionsRange: 5...8, durationRange: 1...5, sectionsDelay: .noDelay))){ updatedProgress in
+                        progress = updatedProgress
+                    }
+                        .frame(width: 150, height: 150)
+                    Text("\(progress)")
+                }
+            case .randomizedConstantDelay:
+                ZStack {
+                    GSProgressBar(type: .circular, animationType: .randomized(configuration: .init(sectionsRange: 5...8, durationRange: 1...5, sectionsDelay: .constantDelay(delay: 1.2)))){ updatedProgress in
+                        progress = updatedProgress
+                    }
+                        .frame(width: 150, height: 150)
+                    Text("\(progress)")
+                }
+            case .randomizedRandomDelay:
+                ZStack {
+                    GSProgressBar(type: .circular, animationType: .randomized(configuration: .init(sectionsRange: 5...8, durationRange: 1...5, sectionsDelay: .randomizedDelay(delayRange: 0.4...5)))) { updatedProgress in
+                            progress = updatedProgress
+                        }
+                        .frame(width: 150, height: 150)
+                    Text("\(progress)")
+                }
+            }
+        }
+    }
+}
+
+struct CircularScreen: View {
+    @State var progress: CGFloat = 0.0
+    init(){
+        print("In Init!")
+    }
+    var body: some View {
+        ZStack {
+            GSProgressBar(type: .circular, animationType: .linear(duration: 5)) { updatedProgress in
+                progress = updatedProgress
+            }
+            .frame(width: 150, height: 150)
+            Text("\(progress)")
+        }
+    }
+}
 #Preview {
-    ContentView()
+    ContentView2()
 }
---
+++ Sources/GSProgressBar/ProgressBars/CircularLoader.swift
@@ -0,0 +1,13 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Dor Ditchi on 27/12/2023.
+//
+
+import SwiftUI
+
+
+#Preview {
+    SwiftUIView()
+}
