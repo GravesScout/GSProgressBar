78dcfbe6399807d8e032f3502f848d1d3bc74ac6
Dor Ditchi <dditchi@digibank.co.il>
2023-12-27 15:52:42 +0200

    Added enums and typeAlias

---
+++ Sources/GSProgressBar/GSProgressBarType.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by Dor Ditchi on 21/12/2023.
+//
+
+import Foundation
+
+public enum GSProgressBarType {
+    case linear
+    case circular
+    case customPath
+}
---
+++ Sources/GSProgressBar/GSProgressUpdater.swift
@@ -0,0 +1,10 @@
+//
+//  File.swift
+//  
+//
+//  Created by Dor Ditchi on 27/12/2023.
+//
+
+import Foundation
+
+public typealias GSProgressUpdater = (CGFloat) -> Void
