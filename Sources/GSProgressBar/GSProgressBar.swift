3fb91fceb89d4eaf9d67bd1f52da668102c888ab
Dor Ditchi <dditchi@digibank.co.il>
2024-01-21 11:50:49 +0200

    Added line width to api.

--- Sources/GSProgressBar/GSProgressBar.swift
+++ Sources/GSProgressBar/GSProgressBar.swift
@@ -6,22 +6,19 @@ import SwiftUI
 import Combine
 
 public struct GSManualProgressBar: View {
-    private let type: GSProgressBarType
+    public let type: GSProgressBarType
+    public let trackLineWidth: CGFloat
+    public let fillLineWidth: CGFloat
     @Binding var progress: CGFloat
-    
-    public init(type: GSProgressBarType,
-                progress: Binding<CGFloat>) {
-        self.type = type
-        _progress = progress
-    }
     
     public var body: some View {
         switch type {
         case .linear:
             EmptyView()
         case .circular:
-            GSCircularProgressBar(progress: $progress)
-                
+            GSCircularProgressBar(progress: $progress,
+                                  trackLineWidth: trackLineWidth,
+                                  fillLineWidth: fillLineWidth)
         case .customPath:
             EmptyView()
         }
@@ -30,23 +27,29 @@ public struct GSManualProgressBar: View {
 
 
 public struct GSProgressBar: View, Equatable {
-    private let progressUpdater: GSProgressUpdater?
-    private let type: GSProgressBarType
-    private let animationType: GSAnimationType
-    @Binding private var play: Bool
+    public let type: GSProgressBarType
+    public let animationType: GSAnimationType
+    public let trackLineWidth: CGFloat
+    public let fillLineWidth: CGFloat
+    @Binding public var play: Bool
+    public var progressUpdater: GSProgressUpdater? = .none
     
-    public init(type: GSProgressBarType,
-                animationType: GSAnimationType,
-                play: Binding<Bool>,
-                progressUpdater: GSProgressUpdater? = nil) {
+    public init(type: GSProgressBarType, animationType: GSAnimationType, trackLineWidth: CGFloat, fillLineWidth: CGFloat, play: Binding<Bool>, progressUpdater: GSProgressUpdater? = nil) {
         self.type = type
         self.animationType = animationType
+        self.trackLineWidth = trackLineWidth
+        self.fillLineWidth = fillLineWidth
         self.progressUpdater = progressUpdater
         _play = play
     }
     
     public var body: some View {
-        GSProgressBarWrapper(type: type, animationType: animationType, progressUpdater: progressUpdater, play: $play)
+        GSProgressBarWrapper(type: type, 
+                             animationType: animationType,
+                             progressUpdater: progressUpdater,
+                             play: $play, 
+                             trackLineWidth: trackLineWidth,
+                             fillLineWidth: fillLineWidth)
     }
     
     public static func == (lhs: GSProgressBar, rhs: GSProgressBar) -> Bool {
@@ -58,6 +61,8 @@ struct GSProgressBarWrapper: View {
     private let progressUpdater: GSProgressUpdater?
     private let type: GSProgressBarType
     private let configuration: GSProgressBarConfiguration
+    private let trackLineWidth: CGFloat
+    private let fillLineWidth: CGFloat
     @Binding private var play: Bool
     
     @State private var animationTicker: Timer.TimerPublisher = .init(interval: 1/60, runLoop: .main, mode: .common)
@@ -82,10 +87,14 @@ struct GSProgressBarWrapper: View {
     init(type: GSProgressBarType,
          animationType: GSAnimationType,
          progressUpdater: GSProgressUpdater? = nil,
-         play: Binding<Bool>) {
+         play: Binding<Bool>,
+         trackLineWidth: CGFloat,
+         fillLineWidth: CGFloat) {
         self.type = type
         self.configuration = .init(progressAnimationConfiguration: animationType)
         self.progressUpdater = progressUpdater
+        self.trackLineWidth = trackLineWidth
+        self.fillLineWidth = fillLineWidth
         _play = play
         _currentSection = State(initialValue:configuration.sectionsDurations[0])
         _nextStopValue = State(initialValue:currentSection.sectionProportionValue)
@@ -96,7 +105,9 @@ struct GSProgressBarWrapper: View {
         case .linear:
             EmptyView()
         case .circular:
-            GSCircularProgressBar(progress: $progress)
+            GSCircularProgressBar(progress: $progress,
+                                  trackLineWidth: trackLineWidth,
+                                  fillLineWidth: fillLineWidth)
                 .onAppear{
                     play ? start() : pause()
                 }
@@ -153,27 +164,49 @@ struct GSProgressBarWrapper: View {
 }
 
 #Preview("Linear") {
-    GSProgressBar(type: .circular, animationType: .linear(duration: 5), play: .constant(true))
+    GSProgressBar(type: .circular,
+                  animationType: .linear(duration: 5),
+                  trackLineWidth: 16,
+                  fillLineWidth: 14,
+                  play: .constant(true))
     .frame(width: 150, height: 150)
 }
 #Preview("Sectioned") {
-    GSProgressBar(type: .circular, animationType: .sectioned(sections: [
-        .init(duration: 3, sectionProportionValue: 0.3, sectionDelay: 2),
-            .init(duration: 1.5, sectionProportionValue: 0.6, sectionDelay: 4),
-            .init(duration: 5, sectionProportionValue: 0.1)]), play: .constant(true))
+    GSProgressBar(
+        type: .circular,
+        animationType: .sectioned(
+            sections: [
+                .init(duration: 3, sectionProportionValue: 0.3, sectionDelay: 2),
+                .init(duration: 1.5, sectionProportionValue: 0.6, sectionDelay: 4),
+                .init(duration: 5, sectionProportionValue: 0.1)]),
+        trackLineWidth: 16,
+        fillLineWidth: 14,
+        play: .constant(true))
     .frame(width: 150, height: 150)
 }
 #Preview("Randomized no delay") {
-    GSProgressBar(type: .circular, animationType: .randomized(configuration: .init(sectionsRange: 2...2, durationRange: 1...5, sectionsDelay: .noDelay)), play: .constant(true))
+    GSProgressBar(type: .circular, 
+                  animationType: .randomized(configuration: .init(sectionsRange: 2...2, durationRange: 1...5, sectionsDelay: .noDelay)),
+                  trackLineWidth: 16,
+                  fillLineWidth: 14,
+                  play: .constant(true))
     .frame(width: 150, height: 150)
 }
 #Preview("Randomized constant delay") {
-    GSProgressBar(type: .circular, animationType: .randomized(configuration: .init(sectionsRange: 5...8, durationRange: 1...5, sectionsDelay: .constantDelay(delay: 1.2))), play: .constant(true))
+    GSProgressBar(type: .circular, 
+                  animationType: .randomized(configuration: .init(sectionsRange: 5...8, durationRange: 1...5, sectionsDelay: .constantDelay(delay: 1.2))),
+                  trackLineWidth: 16,
+                  fillLineWidth: 14,
+                  play: .constant(true))
     .frame(width: 150, height: 150)
     
 }
 #Preview("Randomized random delay") {
-    GSProgressBar(type: .circular, animationType: .randomized(configuration: .init(sectionsRange: 5...8, durationRange: 1...5, sectionsDelay: .randomizedDelay(delayRange: 0.4...5))), play: .constant(true))
+    GSProgressBar(type: .circular, 
+                  animationType: .randomized(configuration: .init(sectionsRange: 5...8, durationRange: 1...5, sectionsDelay: .randomizedDelay(delayRange: 0.4...5))),
+                  trackLineWidth: 16,
+                  fillLineWidth: 14,
+                  play: .constant(true))
     
     .frame(width: 150, height: 150)
 }
