8a4170d02379563b6627cb8b098785af8d288307
Dor Ditchi <dditchi@digibank.co.il>
2024-01-21 14:14:42 +0200

    Created linear progress bar.

---
+++ Sources/GSProgressBar/ProgressBars/GSLinearProgressBar.swift
@@ -0,0 +1,18 @@
+//
+//  GSLinearProgressBar.swift
+//
+//
+//  Created by Dor Ditchi on 21/01/2024.
+//
+
+import SwiftUI
+
+struct GSLinearProgressBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GSLinearProgressBar()
+}
