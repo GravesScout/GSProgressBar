bedb64b5e1ffec1bebafd737f16fe03efec4b9c5
Dor Ditchi <dditchi@digibank.co.il>
2024-01-21 14:36:08 +0200

    Added linear progress bar implementation.

--- Sources/GSProgressBar/ProgressBars/GSLinearProgressBar.swift
+++ Sources/GSProgressBar/ProgressBars/GSLinearProgressBar.swift
@@ -8,11 +8,32 @@
 import SwiftUI
 
 struct GSLinearProgressBar: View {
+    @State private var maxWidth: CGFloat = 0
+    @Binding var progress: CGFloat
+    let trackLineWidth: CGFloat
+    let fillLineWidth: CGFloat
+    let cornerRadius: CGFloat
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { proxy in
+            ZStack(alignment: .leading) {
+                RoundedRectangle(cornerRadius: cornerRadius)
+                    .frame(height: trackLineWidth)
+                    .foregroundColor(.gray)
+                    .shadow(color:.blue, radius: 5)
+                RoundedRectangle(cornerRadius: cornerRadius)
+                    .frame(width:(maxWidth * progress), height: fillLineWidth)
+                    .foregroundColor(.blue)
+                    .padding(.horizontal, (trackLineWidth-fillLineWidth)/2)
+            }
+            .onAppear {
+                maxWidth = proxy.size.width
+            }
+        }
     }
 }
 
 #Preview {
-    GSLinearProgressBar()
+    GSLinearProgressBar(progress: .constant(0.4), trackLineWidth: 16, fillLineWidth: 14, cornerRadius: 16)
+        .frame(width: 200)
 }
