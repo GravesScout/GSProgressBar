517e8f21cd8564c813785bb66d578cd0fe702d0a
Dor Ditchi <dditchi@digibank.co.il>
2024-01-28 23:54:51 +0200

    Added documentation for GSRandomizedConfiguration.

--- Sources/GSProgressBar/Configurations/GSRandomizedConfiguration.swift
+++ Sources/GSProgressBar/Configurations/GSRandomizedConfiguration.swift
@@ -7,11 +7,17 @@
 
 import Foundation
 
+///  This struct configures the ruleset for randomizing the progress of a ``GSProgressBar``
 public struct GSRandomizedConfiguration {
     public let sectionsRange: ClosedRange<Int>
     public let durationRange: ClosedRange<Double>
     public let sectionsDelay: GSRandomizedDelay
-    
+        
+    /// This initialize a randomized configuration.
+    /// - Parameters:
+    ///   - sectionsRange: A closed range to use for randomizing the amount of sections of progress.
+    ///   - durationRange: A closed range to use for randomizing the durations of the sections in the progress bar.
+    ///   - sectionsDelay: A ``GSRandomizedDelay`` to use for the delays after each section.
     public init(sectionsRange: ClosedRange<Int>, durationRange: ClosedRange<Double>, sectionsDelay: GSRandomizedDelay) {
         self.sectionsRange = sectionsRange
         self.durationRange = durationRange
